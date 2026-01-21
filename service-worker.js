0da7c9854eb87fcbcbde9eec6d2f44c7aa2da498
Skreebl <pamzes3@gmail.com>
2026-01-21 13:22:34 +0000

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("wg-cache").then(cache =>
+      cache.addAll(["./"])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
